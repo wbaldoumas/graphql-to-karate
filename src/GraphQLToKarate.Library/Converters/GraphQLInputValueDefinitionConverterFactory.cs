83c82b83a0461cab30ac4275f7c25339770da869
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2023-01-24 18:44:06 -0800

    General Cleanup (#21)

---
+++ src/GraphQLToKarate.Library/Converters/GraphQLInputValueDefinitionConverterFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace GraphQLToKarate.Library.Converters;
+
+/// <inheritdoc cref="IGraphQLInputValueDefinitionConverterFactory"/>
+[ExcludeFromCodeCoverage]
+public sealed class GraphQLInputValueDefinitionConverterFactory : IGraphQLInputValueDefinitionConverterFactory
+{
+    public IGraphQLInputValueDefinitionConverter Create() => new GraphQLInputValueDefinitionConverter();
+}
--- src/GraphQLToKarate.Library/Features/ScenarioBuilder.cs
+++ src/GraphQLToKarate.Library/Features/ScenarioBuilder.cs
@@ -8,38 +8,33 @@ namespace GraphQLToKarate.Library.Features;
 /// <inheritdoc cref="IScenarioBuilder"/>
 public sealed class ScenarioBuilder : IScenarioBuilder
 {
-    private const int SingleIndent = 2;
-    private const int DoubleIndent = SingleIndent * 2;
-    private const int TripleIndent = SingleIndent * 3;
-    private const int QuadrupleIndent = SingleIndent * 4;
-
     public string Build(GraphQLQueryFieldType graphQLQueryFieldType)
     {
         var stringBuilder = new StringBuilder();
 
         stringBuilder.AppendLine($"Scenario: Perform a {graphQLQueryFieldType.Name} query and validate the response");
-        stringBuilder.AppendLine($"* text query = {SchemaToken.TripleQuote}".Indent(SingleIndent));
-        stringBuilder.AppendLine(graphQLQueryFieldType.QueryString.Indent(TripleIndent));
-        stringBuilder.AppendLine($"{SchemaToken.TripleQuote}".Indent(DoubleIndent));
+        stringBuilder.AppendLine($"* text query = {SchemaToken.TripleQuote}".Indent(Indent.Single));
+        stringBuilder.AppendLine(graphQLQueryFieldType.QueryString.Indent(Indent.Triple));
+        stringBuilder.AppendLine($"{SchemaToken.TripleQuote}".Indent(Indent.Double));
 
         if (graphQLQueryFieldType.Arguments.Any())
         {
             stringBuilder.AppendLine();
-            stringBuilder.AppendLine($"* text variables = {SchemaToken.TripleQuote}".Indent(SingleIndent));
-            stringBuilder.AppendLine("{".Indent(TripleIndent));
+            stringBuilder.AppendLine($"* text variables = {SchemaToken.TripleQuote}".Indent(Indent.Single));
+            stringBuilder.AppendLine("{".Indent(Indent.Triple));
 
             foreach (var argumentVariable in graphQLQueryFieldType.Arguments)
             {
-                stringBuilder.AppendLine($"\"{argumentVariable.VariableName}\": <some value>".Indent(QuadrupleIndent));
+                stringBuilder.AppendLine($"\"{argumentVariable.VariableName}\": <some value>".Indent(Indent.Quadruple));
             }
 
-            stringBuilder.AppendLine("}".Indent(TripleIndent));
-            stringBuilder.AppendLine($"{SchemaToken.TripleQuote}".Indent(DoubleIndent));
+            stringBuilder.AppendLine("}".Indent(Indent.Triple));
+            stringBuilder.AppendLine($"{SchemaToken.TripleQuote}".Indent(Indent.Double));
         }
 
         stringBuilder.AppendLine();
-        stringBuilder.AppendLine("Given path \"/graphql\"".Indent(SingleIndent));
-        stringBuilder.Append("And request ".Indent(SingleIndent));
+        stringBuilder.AppendLine("Given path \"/graphql\"".Indent(Indent.Single));
+        stringBuilder.Append("And request ".Indent(Indent.Single));
         stringBuilder.Append("{ ");
         stringBuilder.Append("query: query, ");
         stringBuilder.Append($"operationName: \"{graphQLQueryFieldType.OperationName}\"");
@@ -50,12 +45,12 @@ public sealed class ScenarioBuilder : IScenarioBuilder
         }
 
         stringBuilder.AppendLine(" }");
-        stringBuilder.AppendLine("When method post".Indent(SingleIndent));
-        stringBuilder.AppendLine("Then status 200".Indent(SingleIndent));
+        stringBuilder.AppendLine("When method post".Indent(Indent.Single));
+        stringBuilder.AppendLine("Then status 200".Indent(Indent.Single));
 
         stringBuilder.Append(graphQLQueryFieldType.IsListReturnType
-            ? $"And match each response.data.{graphQLQueryFieldType.Name} == {graphQLQueryFieldType.ReturnTypeName.FirstCharToLower()}Schema".Indent(SingleIndent)
-            : $"And match response.data.{graphQLQueryFieldType.Name} == {graphQLQueryFieldType.ReturnTypeName.FirstCharToLower()}Schema".Indent(SingleIndent)
+            ? $"And match each response.data.{graphQLQueryFieldType.Name} == {graphQLQueryFieldType.ReturnTypeName.FirstCharToLower()}Schema".Indent(Indent.Single)
+            : $"And match response.data.{graphQLQueryFieldType.Name} == {graphQLQueryFieldType.ReturnTypeName.FirstCharToLower()}Schema".Indent(Indent.Single)
         );
 
         return stringBuilder.ToString();
