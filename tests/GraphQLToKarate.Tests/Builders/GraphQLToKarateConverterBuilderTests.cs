6a4c11fc556ca1143dad3e69cae52bcd10abf354
Will Baldoumas <william.baldoumas@gmail.com>
2023-01-29 16:24:24 -0800

    Coverage

---
+++ tests/GraphQLToKarate.Tests/Builders/GraphQLToKarateConverterBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using GraphQLToKarate.Library.Builders;
+using GraphQLToKarate.Library.Converters;
+using NUnit.Framework;
+
+namespace GraphQLToKarate.Tests.Builders;
+
+[TestFixture]
+internal sealed class GraphQLToKarateConverterBuilderTests
+{
+    [Test]
+    public void Build_builds_expected_converter()
+    {
+        // arrange
+        var subjectUnderTest = new GraphQLToKarateConverterBuilder();
+
+        // act
+        var graphQLToKarateConverter = subjectUnderTest.Build();
+
+        // assert
+        graphQLToKarateConverter.Should().BeOfType<GraphQLToKarateConverter>();
+    }
+}
