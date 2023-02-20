b6aa360f89b99e66a490f63d6b790d84ea24bc01
Will Baldoumas <william.baldoumas@gmail.com>
2023-02-19 16:31:01 -0800

    Cleanup

--- src/GraphQLToKarate.Library/Converters/GraphQLScalarToExampleValueConverter.cs
+++ src/GraphQLToKarate.Library/Converters/GraphQLScalarToExampleValueConverter.cs
@@ -36,7 +36,7 @@ internal sealed class GraphQLScalarToExampleValueConverter : IGraphQLScalarToExa
 
     private string GenerateRandomInt() => _random.Next(MinRandomIntValue, MaxRandomIntValue).ToString();
 
-    private string GenerateRandomFloat() => $"{Math.Round(_random.NextDouble() * (MaxRandomFloatValue - MinRandomFloatValue) + MinRandomFloatValue, 2)}";
+    private string GenerateRandomFloat() => $"{_random.NextDouble() * (MaxRandomFloatValue - MinRandomFloatValue) + MinRandomFloatValue:N2}";
 
     private string GenerateRandomBoolean() => _random.Next(0, 1) == 0 ? "true" : "false";
 
--- src/GraphQLToKarate.Library/Converters/IGraphQLInputValueToExampleValueConverter.cs
+++ src/GraphQLToKarate.Library/Converters/IGraphQLInputValueToExampleValueConverter.cs
@@ -8,6 +8,12 @@ namespace GraphQLToKarate.Library.Converters;
 /// </summary>
 public interface IGraphQLInputValueToExampleValueConverter
 {
+    /// <summary>
+    ///     Converts a <see cref="GraphQLInputValueDefinition"/> to an example value.
+    /// </summary>
+    /// <param name="graphQLInputValueDefinition">The <see cref="GraphQLInputValueDefinition"/> to convert. </param>
+    /// <param name="graphQLDocumentAdapter">The <see cref="IGraphQLDocumentAdapter"/> to use. </param>
+    /// <returns>The converted example value.</returns>
     string Convert(
         GraphQLInputValueDefinition graphQLInputValueDefinition,
         IGraphQLDocumentAdapter graphQLDocumentAdapter
