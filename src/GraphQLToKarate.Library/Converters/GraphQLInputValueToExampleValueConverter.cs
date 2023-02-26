85baecf3d6683cb232425b41174013ea91be873c
Will Baldoumas <william.baldoumas@gmail.com>
2023-02-25 17:12:49 -0800

    Handle Recursive Input Types

--- src/GraphQLToKarate.Library/Converters/GraphQLInputValueToExampleValueConverter.cs
+++ src/GraphQLToKarate.Library/Converters/GraphQLInputValueToExampleValueConverter.cs
@@ -4,6 +4,8 @@ using GraphQLToKarate.Library.Extensions;
 using GraphQLToKarate.Library.Tokens;
 using System.Text;
 using GraphQLToKarate.Library.Exceptions;
+using QuikGraph;
+using QuikGraph.Algorithms;
 
 namespace GraphQLToKarate.Library.Converters;
 
@@ -21,14 +23,24 @@ internal sealed class GraphQLInputValueToExampleValueConverter : IGraphQLInputVa
         IGraphQLDocumentAdapter graphQLDocumentAdapter
     ) => Convert(graphQLInputValueDefinition.Type, graphQLDocumentAdapter);
 
-    private string Convert(GraphQLType graphQLType, IGraphQLDocumentAdapter graphQLDocumentAdapter) => graphQLType switch
+    private string Convert(
+        GraphQLInputValueDefinition graphQLInputValueDefinition,
+        IGraphQLDocumentAdapter graphQLDocumentAdapter,
+        AdjacencyGraph<string, Edge<string>> inputValueRelationships
+    ) => Convert(graphQLInputValueDefinition.Type, graphQLDocumentAdapter, inputValueRelationships);
+
+    private string Convert(
+        GraphQLType graphQLType, 
+        IGraphQLDocumentAdapter graphQLDocumentAdapter,
+        AdjacencyGraph<string, Edge<string>>? inputValueRelationships = null) => graphQLType switch
     {
-        GraphQLListType graphQLListType => $"[ {Convert(graphQLListType.Type, graphQLDocumentAdapter)} ]",
-        GraphQLNonNullType graphQLNonNullType => Convert(graphQLNonNullType.Type, graphQLDocumentAdapter),
+        GraphQLListType graphQLListType => $"[ {Convert(graphQLListType.Type, graphQLDocumentAdapter, inputValueRelationships)} ]",
+        GraphQLNonNullType graphQLNonNullType => Convert(graphQLNonNullType.Type, graphQLDocumentAdapter, inputValueRelationships),
         GraphQLNamedType graphQLNamedType when
             graphQLDocumentAdapter.IsGraphQLInputObjectTypeDefinition(graphQLNamedType.GetTypeName()) => Convert(
                 graphQLDocumentAdapter.GetGraphQLInputObjectTypeDefinition(graphQLNamedType.GetTypeName())!,
-                graphQLDocumentAdapter
+                graphQLDocumentAdapter,
+                inputValueRelationships ?? new AdjacencyGraph<string, Edge<string>>()
             ),
         GraphQLNamedType graphQLNamedType => _graphQLScalarToExampleValueConverter.Convert(
             graphQLNamedType,
@@ -39,20 +51,47 @@ internal sealed class GraphQLInputValueToExampleValueConverter : IGraphQLInputVa
 
     private string Convert(
         GraphQLInputObjectTypeDefinition graphQLInputObjectTypeDefinition,
-        IGraphQLDocumentAdapter graphQLDocumentAdapter)
+        IGraphQLDocumentAdapter graphQLDocumentAdapter,
+        AdjacencyGraph<string, Edge<string>> inputValueRelationships)
     {
         var stringBuilder = new StringBuilder();
 
-        stringBuilder.Append("{ ");
+        stringBuilder.Append($"{SchemaToken.OpenBrace} ");
+
+        var parentInputValueDefinitionTypeName = graphQLInputObjectTypeDefinition.NameValue();
+
+        inputValueRelationships.AddVertex(parentInputValueDefinitionTypeName);
 
         foreach (var graphQLInputValueDefinition in graphQLInputObjectTypeDefinition.Fields!)
         {
-            stringBuilder.Append($"\"{graphQLInputValueDefinition.NameValue()}\": {Convert(graphQLInputValueDefinition, graphQLDocumentAdapter)}{SchemaToken.Comma} ");
+            var childInputValueName = graphQLInputValueDefinition.NameValue();
+            var childInputValueDefinitionTypeName = graphQLInputValueDefinition.Type.GetTypeName();
+
+            inputValueRelationships.AddVertex(childInputValueDefinitionTypeName);
+
+            var edge = new Edge<string>(parentInputValueDefinitionTypeName, childInputValueDefinitionTypeName);
+
+            inputValueRelationships.AddEdge(edge);
+
+            // If adding the edge generates a cycle, remove it and generate a placeholder value to prevent infinite recursion...
+            if (!inputValueRelationships.IsDirectedAcyclicGraph())
+            {
+                inputValueRelationships.RemoveEdge(edge);
+
+                stringBuilder.Append(
+                    graphQLInputValueDefinition.Type is GraphQLListType or GraphQLNonNullType { Type: GraphQLListType }
+                        ? $"\"{childInputValueName}\": [ <some {childInputValueDefinitionTypeName} value> ]{SchemaToken.Comma} "
+                        : $"\"{childInputValueName}\": <some {childInputValueDefinitionTypeName} value>{SchemaToken.Comma} "
+                );
+
+                continue;
+            }
+
+            stringBuilder.Append($"\"{childInputValueName}\": {Convert(graphQLInputValueDefinition, graphQLDocumentAdapter, inputValueRelationships)}{SchemaToken.Comma} ");
         }
 
         stringBuilder.TrimEnd(2); // Remove the trailing comma and space.
-
-        stringBuilder.Append(" }");
+        stringBuilder.Append($" {SchemaToken.CloseBrace}");
 
         return stringBuilder.ToString();
     }
--- tests/GraphQLToKarate.Tests/Converters/GraphQLInputValueToExampleValueConverterTests.cs
+++ tests/GraphQLToKarate.Tests/Converters/GraphQLInputValueToExampleValueConverterTests.cs
@@ -267,6 +267,178 @@ internal sealed class GraphQLInputValueToExampleValueConverterTests
     }
 
     [Test]
+    public void Convert_returns_expected_example_value_for_recursive_input_type()
+    {
+        // arrange
+        var nestedGraphQLInputObjectTypeDefinition = new GraphQLInputObjectTypeDefinition
+        {
+            Name = new GraphQLName("TestNestedInputObjectTypeDefinition"),
+            Fields = new GraphQLInputFieldsDefinition
+            {
+                Items = new List<GraphQLInputValueDefinition>
+                {
+                    new()
+                    {
+                        Name = new GraphQLName("nestedValue"),
+                        Type = new GraphQLNamedType
+                        {
+                            Name = new GraphQLName(GraphQLToken.Int)
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("recursiveNestedValue"),
+                        Type = new GraphQLNamedType
+                        {
+                            Name = new GraphQLName("TestInputObjectTypeDefinition")
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("recursiveNestedListValue"),
+                        Type = new GraphQLListType
+                        {
+                            Type = new GraphQLNamedType
+                            {
+                                Name = new GraphQLName("TestInputObjectTypeDefinition")
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        var graphQLInputObjectTypeDefinition = new GraphQLInputObjectTypeDefinition
+        {
+            Name = new GraphQLName("TestInputObjectTypeDefinition"),
+            Fields = new GraphQLInputFieldsDefinition
+            {
+                Items = new List<GraphQLInputValueDefinition>
+                {
+                    new()
+                    {
+                        Name = new GraphQLName("exampleValue"),
+                        Type = new GraphQLNamedType
+                        {
+                            Name = new GraphQLName(GraphQLToken.String)
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("exampleValue2"),
+                        Type = new GraphQLListType
+                        {
+                            Type = new GraphQLNamedType
+                            {
+                                Name = new GraphQLName(GraphQLToken.Int)
+                            }
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("exampleValue3"),
+                        Type = new GraphQLNamedType
+                        {
+                            Name = new GraphQLName(GraphQLToken.Float)
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("exampleValue4"),
+                        Type = new GraphQLNamedType
+                        {
+                            Name = new GraphQLName(GraphQLToken.Boolean)
+                        }
+                    },
+                    new()
+                    {
+                        Name = new GraphQLName("exampleValue5"),
+                        Type = new GraphQLNonNullType
+                        {
+                            Type = new GraphQLNamedType
+                            {
+                                Name = new GraphQLName(nestedGraphQLInputObjectTypeDefinition.Name)
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        var graphQLInputValueDefinition = new GraphQLInputValueDefinition
+        {
+            Name = new GraphQLName("exampleInput"),
+            Type = new GraphQLNamedType
+            {
+                Name = new GraphQLName(graphQLInputObjectTypeDefinition.NameValue())
+            }
+        };
+
+        _mockGraphQLDocumentAdapter!
+            .IsGraphQLInputObjectTypeDefinition(graphQLInputObjectTypeDefinition.NameValue())
+            .Returns(true);
+
+        _mockGraphQLDocumentAdapter!
+            .IsGraphQLInputObjectTypeDefinition(nestedGraphQLInputObjectTypeDefinition.NameValue())
+            .Returns(true);
+
+        _mockGraphQLDocumentAdapter
+            .GetGraphQLInputObjectTypeDefinition(graphQLInputObjectTypeDefinition.NameValue())
+            .Returns(graphQLInputObjectTypeDefinition);
+
+        _mockGraphQLDocumentAdapter
+            .GetGraphQLInputObjectTypeDefinition(nestedGraphQLInputObjectTypeDefinition.NameValue())
+            .Returns(nestedGraphQLInputObjectTypeDefinition);
+
+        const string exampleStringValue = "\"an example value\"";
+        const string exampleIntValue = "1";
+        const string exampleFloatValue = "1.0";
+        const string exampleBooleanValue = "true";
+
+        _mockGraphQLScalarToExampleValueConverter!
+            .Convert(
+                Arg.Is<GraphQLType>(
+                    graphQLType => (graphQLType as GraphQLNamedType)!.NameValue() == GraphQLToken.String),
+                Arg.Any<IGraphQLDocumentAdapter>()
+            )
+            .Returns(exampleStringValue);
+
+        _mockGraphQLScalarToExampleValueConverter!
+            .Convert(
+                Arg.Is<GraphQLType>(graphQLType => (graphQLType as GraphQLNamedType)!.NameValue() == GraphQLToken.Int),
+                Arg.Any<IGraphQLDocumentAdapter>()
+            )
+            .Returns(exampleIntValue);
+
+        _mockGraphQLScalarToExampleValueConverter!
+            .Convert(
+                Arg.Is<GraphQLType>(graphQLType =>
+                    (graphQLType as GraphQLNamedType)!.NameValue() == GraphQLToken.Float),
+                Arg.Any<IGraphQLDocumentAdapter>()
+            )
+            .Returns(exampleFloatValue);
+
+        _mockGraphQLScalarToExampleValueConverter!
+            .Convert(
+                Arg.Is<GraphQLType>(graphQLType =>
+                    (graphQLType as GraphQLNamedType)!.NameValue() == GraphQLToken.Boolean),
+                Arg.Any<IGraphQLDocumentAdapter>()
+            )
+            .Returns(exampleBooleanValue);
+
+        const string expectedExampleValue = $"{{ \"exampleValue\": {exampleStringValue}, \"exampleValue2\": [ {exampleIntValue} ], \"exampleValue3\": {exampleFloatValue}, \"exampleValue4\": {exampleBooleanValue}, \"exampleValue5\": {{ \"nestedValue\": {exampleIntValue}, \"recursiveNestedValue\": <some TestInputObjectTypeDefinition value>, \"recursiveNestedListValue\": [ <some TestInputObjectTypeDefinition value> ] }} }}";
+
+        // act
+        var exampleValue = _subjectUnderTest!.Convert(
+            graphQLInputValueDefinition,
+            _mockGraphQLDocumentAdapter
+        );
+
+        // assert
+        exampleValue.Should().Be(expectedExampleValue);
+    }
+
+    [Test]
     public void Convert_throws_exception_when_unsupported_GraphQLType_is_encountered()
     {
         // arrange
