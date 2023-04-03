d0788ca55587b4c3795dae79907ac7d1de64c7db
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2023-04-03 15:24:02 -0700

    Integration Tests (#83)

---
+++ tests/GraphQLToKarate.Integration.Api/Types/Query.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphQLToKarate.Integration.Api.Types;
+
+public sealed class Query
+{
+    public User GetUserById([GraphQLType(typeof(IdType))][GraphQLNonNullType] string id) => new()
+    {
+        Id = id,
+        Name = "John Doe",
+        Role = UserRole.Administrator,
+        CreatedAt = DateTime.UtcNow,
+        UpdatedAt = DateTime.UtcNow
+    };
+}
